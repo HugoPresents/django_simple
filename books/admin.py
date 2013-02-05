f5f3016dedeec23e93668eb5d7b932dddd6407e2
Rabbit_52 <rabbitzhang52@gmail.com>
2013-02-05 11:57:40 +0800

    Django admin and again~

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from books.models import Publisher, Author, Book
 
+# define customize list style
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'publication_date', 'publisher')
+    # use this line to support filter results
+    # don't forget "," end of the line!
+    list_filter = ('publication_date',)
+    # I'm not fully understand here~
+    date_hierarchy = 'publication_date'
 admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
+# register customize class
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
